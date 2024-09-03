b83e055e595fb9885ec41d43bc9e63970a797d63
Suresh Packiry <sureshpackiry73@gmail.com>
2024-09-04 09:51:42 +1000

    Add: More components and modules

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # My first SPA while learning React Router and Context API
 
 Using vite v.4 and react router v.6. In the classes css modules are explained and shows how if there are more than one nav or ul, css makes a unique id or classname for those elements.
+
+Added a Snippet for the css module to save time importing the css files
--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,22 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>LIST</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
