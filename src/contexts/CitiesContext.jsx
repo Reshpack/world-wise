1f5a2e30d9848f5359879348b15409903fb4a80b
Suresh Packiry <sureshpackiry73@gmail.com>
2024-09-05 18:08:22 +1000

    Add: Context API with named values

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,67 @@
+import { createContext, useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+
+const BASE_URL = "http://localhost:9000";
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState();
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        console.log("there is an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("there is an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+CitiesProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CititesContext was used outside the CititesProvider");
+
+  return context;
+}
+
+export { CitiesProvider, useCities };
